df44e1127f3cd6f5c65c74c11d341c8abc49485d
alvarowrl <alvaro29miguel@gmail.com>
2026-02-04 10:35:46 -0300

    Add files via upload

---
+++ LinguagemProgramação/Exercico1/src/ClienteDAO.java
@@ -0,0 +1,13 @@
+
+
+
+
+public class ClienteDAO {
+    
+    public String mostrarNomeRg(ClienteDTO clienteDTO){
+        return "O nome informado foi: " + clienteDTO.getNome() + "\nO RG informado foi: " + clienteDTO.getRg();
+    }
+    public String mostrarNomeIdade(ClienteDTO clienteDTO){
+        return "O nome informado foi: " + clienteDTO.getNome() + "\nA Idade informada foi: " + clienteDTO.getIdade();
+    }
+}
