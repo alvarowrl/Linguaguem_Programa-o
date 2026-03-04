aabd9fe7f29e7a311e01dc1eeb7b28727184a8f9
alvarowrl <alvaro29miguel@gmail.com>
2026-03-04 10:24:46 -0300

    Add files via upload

---
+++ Projeto_1/src/br/com/projeto_1/ctr/ClienteCTR.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.projeto_1.ctr;
+
+import java.sql.*;
+import br.com.projeto_1.dto.ClienteDTO;
+import br.com.projeto_1.dao.ClienteDAO;
+import br.com.projeto_1.dao.ConexaoDAO;
+
+public class ClienteCTR {
+    ClienteDAO clienteDAO = new ClienteDAO();
+    public ClienteCTR(){
+    }
+    public String inserirCliente(ClienteDTO clienteDTO){
+        try{
+            if(clienteDAO.inserirCliente(clienteDTO)){
+                return "Cliente cadastrado com sucesso!!!";
+            } else { 
+                return "Cliente NÃO cadastrado!!!";
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return "Cliente não cadastrado!!!";
+        }
+        
+    }
+    
+}
